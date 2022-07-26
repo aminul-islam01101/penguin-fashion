aad81dd060a4907a3f3b90eeec98dfb19a23c9c6
aminulislam01101 <aminul.islam01101@gmail.com>
2022-07-26 23:46:42 +0600

    banner section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+
+  
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    container: { 
+      center: true,
+      
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      
+      },
+    },
   },
   plugins: [],
 }
