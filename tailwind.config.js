38be4e534c886e690e53a95aef78386ff67ef1d0
aminulislam01101 <aminul.islam01101@gmail.com>
2022-07-30 03:59:12 +0600

    Navbar added with dark mood toggler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  darkMode: 'class',
+
   content: ["./public/**/*.html"],
+
   theme: {
     container: { 
       center: true,
       
+      
     },
+
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      
+       },
+       width: {
+        '95': '95%',
+        '90': '90%',
+        '85': '85%',
+        '80': '80%',
+        '70': '70%',
+        '60': '60%',
+        '40': '40%',
+
       },
-
-
-
+      maxWidth: {
+        '95': '95%',
+        '90': '90%',
+        '85': '85%',
+        '80': '80%',
+        '70': '70%',
+        '60': '60%',
+        '40': '40%'
       
     },
   },
-  plugins: [],
+  
+  plugins: [
+
+  ]
+}
 }
